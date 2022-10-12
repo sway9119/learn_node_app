a7c80827ac727af034518dcdb9894e5dc1b3ef80
sway <40759948+sway9119@users.noreply.github.com>
2022-10-13 01:51:37 +0900

    メイン処理の追加

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,35 @@ const promptInput = async (text: string) => {
   );
   return input.trim();
 };
+type Mode = "normal" | "hard" | "very hard";
+
+class HitAndBlow {
+  private readonly answerSource = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
+  private answer: string[] = [];
+  private trycount = 0;
+  private mode: Mode = "normal";
+
+  async setting() {
+    this.mode = (await promptInput("モードを入力してください。")) as Mode;
+    const answerLength = this.getAnswerLength();
+
+    while (this.answer.length < answerLength) {
+      const randNum = Math.floor(Math.random() * this.answerSource.length);
+      const selectedItem = this.answerSource[randNum];
+      if (!this.answer.includes(selectedItem)) {
+        this.answer.push(selectedItem);
+      }
+    }
+  }
+
